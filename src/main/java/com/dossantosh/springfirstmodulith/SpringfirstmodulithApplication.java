c587427b4ace0c9607e1db4cbe9edee08bf1a0d5
Sebastian Dos Santos <sebastiandossantosh@gmail.com>
2026-04-24 12:35:20 +0200

    add(HashPassword): medida temporal para pruebas

--- src/main/java/com/dossantosh/springfirstmodulith/SpringfirstmodulithApplication.java
+++ src/main/java/com/dossantosh/springfirstmodulith/SpringfirstmodulithApplication.java
@@ -4,13 +4,24 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.modulith.Modulithic;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Modulithic
 @EnableMethodSecurity
 @SpringBootApplication
 public class SpringfirstmodulithApplication {
 
-	static void main(String[] args) {
+	private static final String HASH_PASSWORD_ARG = "--hash-password=";
+
+	public static void main(String[] args) {
+
+		for (String arg : args) {
+			if (arg != null && arg.startsWith(HASH_PASSWORD_ARG)) {
+				String rawPassword = arg.substring(HASH_PASSWORD_ARG.length());
+				System.out.println(new BCryptPasswordEncoder().encode(rawPassword));
+				return;
+			}
+		}
 
 		SpringApplication.run(SpringfirstmodulithApplication.class, args);
 	}
